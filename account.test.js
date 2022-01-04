3675abe69b9b56f765803b0988af29d6b0c03588
Matt Zimmer <zimmermja@gmail.com>
2022-01-04 16:16:26 +0000

    .deposit tests passing

--- account.test.js
+++ account.test.js
@@ -2,20 +2,28 @@ const Account = require("./account.js");
 
 const testAccount = new Account();
 
-describe("Creating transations", () => {
-  test(".deposit creates a new transaction", () => {
+describe(".deposit", () => {
+  test("Creates a new transaction", () => {
     const testTrans = testAccount.transactions.length;
     testAccount.deposit(10);
     expect(testAccount.transactions.length).toBe(testTrans + 1);
   });
 
-  test(".deposit increases the balance", () => {
-    const testBalance = testAccount.balance;
-    testAccount.deposit(10);
-    expect(testAccount.balance).toBe(testBalance + 10);
-    testAccount.deposit(10.5);
-    expect(testAccount.balance).toBe(testBalance + 10 + 10.5);
-    testAccount.deposit(10.999999999);
-    expect(testAccount.balance).toBe(testBalance + 10 + 10.5 + 10.99);
+  test("Increases the balance", () => {
+    checkDesposit(testAccount, 10);
+  });
+
+  test("Works with decimals", () => {
+    checkDesposit(testAccount, 10.5);
+  });
+
+  test("Works with too many decimals", () => {
+    checkDesposit(testAccount, 10.9999999999, 10.99);
   });
 });
+
+const checkDesposit = (account, mod, expectation = mod) => {
+  account.balance = 0;
+  account.deposit(mod);
+  expect(account.balance).toBe(expectation);
+};
