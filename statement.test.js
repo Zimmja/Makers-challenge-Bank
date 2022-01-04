daf71377774edcfd2c080987a638e5d533e1f171
Matt Zimmer <zimmermja@gmail.com>
2022-01-04 13:44:43 +0000

    set up the test suite with mocks for Statement

--- statement.test.js
+++ statement.test.js
@@ -1,7 +1,31 @@
 const Statement = require("./statement.js");
 
-const testStatement = new Statement();
+const trans01Data = {
+  time: "10/01/2023",
+  balance: "1000.00",
+  credit: "1000.00",
+  debit: null,
+};
+const trans02Data = {
+  time: "13/01/2023",
+  balance: "2000.00",
+  credit: "3000.00",
+  debit: null,
+};
+const trans03Data = {
+  time: "14/01/2023",
+  balance: "1000.00",
+  credit: null,
+  debit: "2500.00",
+};
+
+const trans01 = { getData: () => trans01Data };
+const trans02 = { getData: () => trans02Data };
+const trans03 = { getData: () => trans03Data };
+const sEmpty = new Statement([]);
+const sPart = new Statement([trans01]);
+const sFull = new Statement([trans01, trans02, trans03]);
 
 test("hello world", () => {
-  expect(testStatement.sayHello()).toEqual("Hello world!");
+  expect(sEmpty.printStatement()).toEqual("date || credit || debit || balance");
 });
