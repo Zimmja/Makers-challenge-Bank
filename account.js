d80f11c748db77ec2d199fb2601ab880288f685d
Matt Zimmer <zimmermja@gmail.com>
2022-01-05 11:34:44 +0000

    tidied up names on account test suite, added validation for statements

--- account.js
+++ account.js
@@ -18,13 +18,14 @@ class Account {
   };
 
   statement = (stmnt = new Statement(this.transactions)) => {
+    if (!stmnt.printStatement) throw "ERROR: invalid statement";
     stmnt.printStatement();
   };
 
   // Support functions
   //----------------------------------------------------------------
   beginTransaction = (amount, trsx) => {
-    amount = this.validateInput(amount);
+    amount = this.validateAmount(amount);
     trsx = this.validateTransaction(amount, trsx);
     return this.handleTransaction(trsx);
   };
@@ -44,23 +45,16 @@ class Account {
   validateTransaction = (amount, trsx) => {
     if (trsx === true || trsx === false)
       trsx = new Transaction(this.balance, amount, trsx);
-    if (trsx.getData ? false : true) throw "ERROR: invalid transaction";
+    if (!trsx.getData) throw "ERROR: invalid transaction";
     return trsx;
   };
 
-  validateInput = (amount) => {
+  validateAmount = (amount) => {
     amount = Number(amount);
     if (isNaN(amount)) throw "ERROR: entered value must be a number";
-    if (amount === 0) throw "ERROR: entered value must be greater than 0";
+    if (amount <= 0) throw "ERROR: entered value must be greater than 0";
     return amount;
   };
 }
 
 module.exports = Account;
-
-// const myStatement = new Statement([]);
-// const myTransaction = new Transaction(0, 10, true);
-// const myMock = { getData: () => "hello" };
-// console.log(myStatement.getData ? true : false);
-// console.log(myTransaction.getData ? true : false);
-// console.log(myMock.getData ? true : false);
--- account.test.js
+++ account.test.js
@@ -1,60 +1,69 @@
 const Account = require("./account.js");
 
-const dep01 = {
-  balance: "10.00",
+const transDep15 = { getData: () => depos };
+const depos = {
+  balance: "15.00",
 };
-const with01 = {
+const transWith09 = { getData: () => withd };
+const withd = {
   balance: "6.00",
 };
-const transDep01 = { getData: () => dep01 };
-const transWith01 = { getData: () => with01 };
+const transDum = { getData: () => null };
 const tAcc = new Account();
 
-describe(".deposit and .withdraw", () => {
-  test("Create a new transaction", () => {
-    tAcc.deposit(1, transDep01);
-    tAcc.withdraw(1, transWith01);
+describe(".deposit / .withdraw", () => {
+  beforeEach(() => {
+    tAcc.balance = 0;
+    tAcc.transactions = [];
+  });
+
+  test("Creates a new transaction object that is added to the transactions list", () => {
+    tAcc.deposit(1, transDep15);
+    tAcc.withdraw(1, transWith09);
     expect(tAcc.transactions.length).toBe(2);
   });
 
-  test("Set Account.balance to the balance value of the transaction", () => {
-    expect(tAcc.deposit(1, transDep01)).toEqual(10);
+  test("Sets Account.balance to the balance value of the transaction", () => {
+    expect(tAcc.deposit(1, transDep15)).toEqual(15);
+    expect(tAcc.withdraw(1, transWith09)).toEqual(6);
   });
 
-  test("Returns an error if a non-monetary value is entered", () => {
-    expect(() => tAcc.deposit("Ten", transDep01)).toThrowError(
+  test("Returns an error if a non-numerical value is entered", () => {
+    expect(() => tAcc.deposit("Ten", transDum)).toThrowError(
       "ERROR: entered value must be a number"
     );
-    expect(() => tAcc.deposit("10", transDep01)).not.toThrowError(
-      "ERROR: entered value must be a number"
-    );
   });
 
-  test("Will not accept values of 0 for deposit or withdrawal", () => {
-    expect(() => tAcc.deposit(0, transDep01)).toThrowError(
+  test("Will not accept values of <= 0", () => {
+    expect(() => tAcc.deposit(0, transDum)).toThrowError(
       "ERROR: entered value must be greater than 0"
     );
-    expect(() => tAcc.withdraw([], transDep01)).toThrowError(
+    expect(() => tAcc.withdraw(-1, transDum)).toThrowError(
       "ERROR: entered value must be greater than 0"
     );
   });
 
-  test("Will not accept incorrect transaction objects", () => {
+  test("Will not accept invalid Transaction objects", () => {
     expect(() => tAcc.deposit(10, "transaction")).toThrowError(
       "ERROR: invalid transaction"
     );
   });
 
   test("Checking Transaction objects for 100% coverage", () => {
-    const unmockedAccount = new Account();
-    expect(unmockedAccount.deposit(10)).toEqual(10);
-    expect(unmockedAccount.withdraw(5)).toEqual(5);
+    expect(tAcc.deposit(10)).toEqual(10);
+    expect(tAcc.withdraw(5)).toEqual(5);
   });
 });
 
 describe(".statement", () => {
   test("Returns undefined", () => {
     expect(tAcc.statement({ printStatement: () => true })).toBeUndefined();
+  });
+
+  test("Will not accept invalid Statement objects", () => {
+    expect(() => tAcc.statement("statement")).toThrowError(
+      "ERROR: invalid statement"
+    );
   });
 
   test("Checking Statement objects for 100% coverage", () => {
